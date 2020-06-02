7e12654db46b863fa675e9b0c00bfa56576ee8c0
kevan.chen <kevan.chen@sinopa.com>
2020-06-02 10:54:54 +0800

    First Check in

---
+++ ConsoleGetThreads/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleGetThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.GetThread().ToString());
+            
+            Console.ReadKey();
+        }
+
+        public int GetThread()
+        {
+            int start = 1;
+            int end = 100000;
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (!IsBadSetting(mid))
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            if (!IsBadSetting(start))
+                return start;
+            return end;
+        }
+
+        public bool IsBadSetting(int n)
+        {
+            return ThreadPool.SetMinThreads(n, n);
+        }
+    }
+}
